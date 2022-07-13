2935700c0833f33ae5068cefacf5596a9f23825e
Devin <support@adeept.com>
2022-07-13 10:15:08 +0800

    update

--- server/ultra.py
+++ server/ultra.py
@@ -17,20 +17,27 @@ GPIO.setup(Ec, GPIO.IN)
 
 
 def checkdist():       #Reading distance
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(Tr, GPIO.OUT,initial=GPIO.LOW)
-    GPIO.setup(Ec, GPIO.IN)
-    GPIO.output(Tr, GPIO.HIGH)
-    time.sleep(0.000015)
-    GPIO.output(Tr, GPIO.LOW)
-    while not GPIO.input(Ec):
-        pass
-    t1 = time.time()
-    while GPIO.input(Ec):
-        pass
-    t2 = time.time()
-    return round((t2-t1)*340/2,2)
-    #return (t2-t1)*340/2
+    for i in range(5):  # Remove invalid test results.
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(Tr, GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setup(Ec, GPIO.IN)
+        GPIO.output(Tr, GPIO.LOW)
+        time.sleep(0.000002)
+        GPIO.output(Tr, GPIO.HIGH)
+        time.sleep(0.000015)
+        GPIO.output(Tr, GPIO.LOW)
+        while not GPIO.input(Ec):
+            pass
+        t1 = time.time()
+        while GPIO.input(Ec):
+            pass
+        t2 = time.time()
+        dist = (t2-t1)*340/2
+        if dist > 9 and i < 4:  # 5 consecutive times are invalid data, return the last test data
+            continue
+        else:
+            return (t2-t1)*340/2
 
 # def checkdist():       #Reading distance
 #     GPIO.output(Tr, GPIO.HIGH)
@@ -48,6 +55,7 @@ def checkdist():       #Reading distance
 #     return round((t2-t1)*340/2,2)
 
 if __name__ == '__main__':
-    while 1:
-        print(checkdist())
+    while True:
+        distance = checkdist()*100
+        print("%.2f cm" %distance)
         time.sleep(1)
